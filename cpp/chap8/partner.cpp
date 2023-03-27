364cd5f9dd9fdf9e6f9a655616634f9050924029
parkco34 <parkercorya@gmail.com>
2023-03-27 15:06:12 -0400

    ALmost done with this damn hangman game... Kicking my ass.

--- cpp/chap8/partner.cpp
+++ cpp/chap8/partner.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <cctype>
 
 using namespace std;
 
@@ -22,50 +23,98 @@ string get_blanks(string theWord)
     return blanks;
 }
 
-void updateBlanks(string word, string& blanks, char& guessedLetter)
+void updateBlanks(string theWord, string& blanks, char& guessedLetter)
 {
-    for (int i=0; i<word.length(); i++) {
-        if (guessedLetter == word[i]) {
+    for (int i=0; i < theWord.length(); i++) {
+        if (guessedLetter == theWord[i]) {
             blanks[i] = guessedLetter;
         }
-    }
+     }   
 }
 
-int main()
+// Reads textfile to generate the random word
+string get_word(int max_strings)
 {
-    const int MAX_STRINGS = 20;
-    int count = 0;
     ifstream infile("words.txt");
-    string words[MAX_STRINGS];
-
-    // Generates random seed based on current time
-    srand(time(0));
 
     // Check that file opened correctly
     if (!infile) {
         cout << "Failed to open file" << endl;
-        return 1;
+        return "";
     }
 
+    string words[max_strings];
+    // Generates random seed based on current time
+    srand(time(0));
+    int count = 0;
     string word;
+
     /* Randomly selects words from a textfile of any 
      * number of words and stores them in array    ᕙ(▀̿̿Ĺ̯̿̿▀̿ ̿) ᕗ */
     while(getline(infile, word)) {
-        if (rand() % (count + 1) < MAX_STRINGS) {
+        if (rand() % (count + 1) < max_strings) {
 
-            if (count < MAX_STRINGS) {
+            if (count < max_strings) {
                 words[count] = word;
             }
             else {
-                int randIndx = rand() % MAX_STRINGS;
+                int randIndx = rand() % max_strings;
                 words[randIndx] = word;
             }
         }
         count++;
     }
 
-    string blanks = get_blanks(words[rand() % 20]);
-    cout << "Blanks: " << blanks << endl;
+    return words[rand() % max_strings];
+}
+
+int main()
+{
+    const int MAX_STRINGS = 20;
+    string blanks = get_blanks(get_word(MAX_STRINGS));  // Fill length of word with asterisks
+    string word = get_word(MAX_STRINGS); // Randomly selects word
+    int guesses = word.length();
+    string guessed;  // Holds all the guessed letters
+    char guessedLetter; // Current guessed letter
+
+    // Game loop
+    while(blanks != word && guesses > 0) {
+        cout << "The word: " << blanks << endl;
+        cout << "Guess a letter: " << endl;
+        cin >> guessedLetter;
+        guessedLetter = tolower(guessedLetter);
+
+        // If player gets the word, quit game
+        if (blanks == word) {
+            cout << "Congrats!  You got it!! " << endl;
+            cout << "The word: " << word << endl;
+            break;
+        }
+
+        while(guessed.find(guessedLetter) != string::npos) {
+            cout << "You've already guessed that letter, yo ..." << endl;
+            cin >> guessedLetter;
+            guessedLetter = tolower(guessedLetter);
+            guesses--;
+        }
+ 
+        if (word.find(guessedLetter) != string::npos) {
+            cout << "Nice! You guessed right " << endl;
+            updateBlanks(word, blanks, guessedLetter);
+        }
+        else {
+            cout << "Lo Siento ... " << endl;
+        }
+        guessed += guessedLetter;   // Add guessed letter to the string of guesses
+        guesses--;
+
+        cout << "Guesses: " << guesses << endl;
+    }
+
+    if (word != blanks) {
+        cout << "You're dead!  (╯ ͠° ͟ʖ ͡°)╯┻━┻ " << endl;
+        cout << "\n   Word: " << word << endl;
+    }
 
     return 0;
 }
