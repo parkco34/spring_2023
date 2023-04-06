dc5ead008719c68df104c35809ef7e6195aab3c1
parkco34 <parkercorya@gmail.com>
2023-04-06 07:16:30 -0400

    I'm not doing well at all ...

--- cpp/chap9/exercise9_4.cpp
+++ cpp/chap9/exercise9_4.cpp
@@ -9,6 +9,7 @@
 #include <cmath>
 #include <string>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -17,29 +18,141 @@ struct menuItemType {
     double menuPrice;
 };
 
-void getData(string menuList[8])
+// Read text file and load arrays with corresponding data
+void getData(menuItemType menuList[], const int& MAX_ITEM_COUNT)
 {
-    string line;
-    int count = 0;
     ifstream infile("Ch9_Ex4Data.txt");
+    string line;
 
     // Ensure file is openable
     if (!infile) {
         cerr << "Oops!  Unable to open file." << endl;
     }
 
+    // Add elements to arrays from the text file
+    int itemCount = 0;
     while(getline(infile, line)) {
-        if (count % 2 == 0) {
-            menuList[count++] = line;
-            cout << menuList[count] << endl;
+        if (itemCount % 2 == 0) {
+            menuList[itemCount/2].menuItem = line;
         }
+        else {
+            menuList[itemCount/2].menuPrice = stod(line);
+        }
+        itemCount++;
     }
-    
 }
+
+int showMenu(menuItemType menuList[], const int SIZE, double& price)
+{
+    char selection;
+    bool makeSelection = true;
+    int numOfSelections = 0;
+    int choice;
+    price = 0;
+
+    cout << "Welcome to ParkDaddy's Restaurant " << endl;
+    cout << "----- Today's Menu -----" << endl;
+    cout << fixed <<  setprecision(2);
+  
+    // Display menu
+    for (int i=0; i< SIZE; i++) {
+        cout << i+1 << ": " << left << setw(15) << menuList[i].menuItem << "$" << menuList[i].menuPrice << endl;
+    }
+
+    while(makeSelection && numOfSelections < SIZE) {
+        cout << "You can make up to " << SIZE << " single order selections " << endl;
+        cout << "Do you want to make selection Y/y (Yes), N/n (No): " << endl;
+        cin >> selection;
+
+        if (tolower(selection) == 'y') {
+            int choice; // Choice of item number
+        }
+        else
+            break;
+
+        cout << "Enter item number: (Press any letter-key to quit)" << endl;
+        cin >> choice;
+
+        // Sum selection prices
+        cout << "Your choice: (1-8)" << setw(7) << " " << choice << endl;
+        cout << "Price: " << price << endl;
+        // User selects menu options
+        switch(choice) {
+            case 1:
+                price += menuList[choice-1].menuPrice;
+                break;
+
+            case 2:
+                price += menuList[choice-1].menuPrice;
+                break;
+
+            case 3:
+                price += menuList[choice-1].menuPrice;
+                break;
+
+            case 4:
+                price += menuList[choice-1].menuPrice;
+                break;
+
+            case 5:
+                price += menuList[choice-1].menuPrice;
+                break;
+
+            case 6:
+                price += menuList[choice-1].menuPrice;
+                break;
+
+            case 7:
+                price += menuList[choice-1].menuPrice;
+                break;
+
+            case 8:
+                price += menuList[choice-1].menuPrice;
+                break;
+
+            default:
+                cout << "Goodbye " <<  endl;
+                makeSelection = false;  // Quits
+        }
+        numOfSelections++;
+    }
+
+    return numOfSelections;
+}
+
+// Calculates price and prints the check, including the 5% tax
+void printCheck(menuItemType menuList[], int numOfSelections, double price, double tax=0.05)
+{
+    double taxAmount = price * tax;
+    double totalAmount = price + taxAmount;
+    
+    cout << "\n============================================================== " << endl;
+    cout << setfill(' ') << setw(60) << "Welcome to Johnny's Restaurant\n" << endl;
+    cout << left << setw(30) << "Item" << setw(10) << "Price" << endl;
+    cout << setfill('-') << setw(40) << "" << endl;
+    cout << setfill(' ');
+
+    for (int i = 0; i < numOfSelections; i++) {
+        cout << left << setw(30) << menuList[i].menuItem << right << setw(10) << fixed << setprecision(2) << "$" << menuList[i].menuPrice << endl;
+    }
+
+    cout << setfill('-') << setw(40) << "" << endl;
+    cout << setfill(' ');
+
+    cout << left << setw(30) << "Tax" << right << setw(10) << fixed << setprecision(2) << "$" << taxAmount << endl;
+    cout << left << setw(30) << "Amount Due" << right << setw(10) << fixed << setprecision(2) << "$" << totalAmount << endl;
+    cout << "\n============================================================== " << endl;
+}
+
 
 int main()
 {
-    string menuList[8];
-    getData(menuLIst[8]);
+    double price;
+    const int MAX_ITEM_COUNT = 8;
+    menuItemType menuList[MAX_ITEM_COUNT];
+    getData(menuList, MAX_ITEM_COUNT);
+    int numOfSelections = showMenu(menuList, MAX_ITEM_COUNT, price);
+    printCheck(menuList, numOfSelections, price);
+
     return 0;
 }
---
+++ cpp/chap9/exercise9_6.cpp
@@ -0,0 +1,139 @@
+//Write a program whose main function is merely a collection of variable declarations and function calls. 
+// THIS PROGRAM READS A TEXT AND OUTPUTS THE LETTERS, TOGETHER WITH THEIR COUNTS, as explained below in the function printResult. 
+//(There can be NO GLOBAL VARIABLES! 
+// All information must be passed in and out of the functions. USE A STRUCTURE to store the information.) 
+//
+//Your program must consist of at least the following FUNCTIONS:
+//
+//    - openFile: Opens the input and output files. 
+//        You must pass the file streams as parameters (by reference, of course). 
+//        If the file does not exist, the program should print an appropriate message ("The input file does not exist.") and exit.
+//    
+//    - count: Counts every occurrence of capital letters A-Z and small letters a-z in the text 
+//        file opened in the function openFile. 
+//        This information must go into an ARRAY OF STRUCTURES. 
+//        The array must be passed as a parameter, and the file identifier must also be passed as a parameter.
+//
+//    - printResult: Prints the number of capital letters and small letters, as well as the percentage of 
+//        capital letters for every letter A-Z and the percentage of small letters for every letter a-z. 
+//        The percentages should look like this: "25%".
+//        This information must come from an ARRAY OF STRUCTURES, and this array must be passed as a parameter.
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+const int ALPHABET = 26;
+
+struct letters {
+    int upperCount;
+    int lowerCount;
+};
+
+// function prototypes
+void openFile(ifstream& infile);
+void count(ifstream& infile, letters myLetters[]);
+void printResult(letters myLetters[]);
+
+int main()
+{
+    ifstream infile;
+    openFile(infile);
+    letters myLetters[2 * ALPHABET];
+    count(infile, myLetters);
+}
+
+// Opens file for reading
+void openFile(ifstream& infile)
+{
+    infile.open("Ch9_Ex6Data.txt");
+    // Ensure file can be opened
+    if(!infile) {
+        cerr << "File cannot be opened..." << endl;
+        exit(1);
+    }
+}
+
+/* Counts each occurence of uppper/lower case letters and stores them in 
+ *  a struct array, along with the number of each occurence
+ */
+void count(ifstream& infile, letters myLetters[])
+{
+    char c;
+    while (infile.get(c)) {
+        /* As long as the character is a part of the Alphabet, 
+         * add to corresponding counter, where c - 'A' is the index
+         */
+        if(isalpha(c)) {
+            if(isupper(c)) {
+                myLetters[c - 'A'].upperCount++;
+            }
+            else if (islower(c)) {
+                myLetters[c - 'a'].lowerCount++;
+            }
+            else
+                continue;
+        }
+    }
+}
+
+void printResult(letters myLetters[])
+{
+    cout << setw(10) << "Letter: " << setw(10) << "Uppercase: " << setw(10) << 
+        "Lowercase: " << setw(15) << "Uppercase %" << setw(15) << "Lowercase %" << endl;
+
+    for (int i=0; i < ALPHABET; i++) {
+        // Why is this difficult for me?
+    }
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//#include <iostream>
+//#include <string>
+//#include <iomanip>
+//#include <fstream>
+//
+//using namespace std;
+//
+//struct letters {
+//    char letter;
+//    int count;
+//};
+//
+//// function prototypes
+//int openFile(ifstream& infile);
+//void count(letters myLetters[], string line, const int SIZE);
+//void printResult(letters myLetters[]);
+//
+//int main()
+//{
+//    ifstream infile("Ch9_Ex6Data.txt");
+//
+//    const int LINES = openFile(infile);
+//    letters myLetters[LINES];
+//}
+//
+//// Opens file for reading
+//int openFile(ifstream& infile)
+//{
+//    // Ensure file can be opened
+//    if(!infile) {
+//        cerr << "File cannot be opened..." << endl;
+//    }
+//
+//    string line;
+//    int totalLines = 0;
+//    while(getline(infile, line)) {
+//        totalLines++;
+//    }
+//    return totalLines;
+//}
+//
+//void count(letters myLetters[], string line, const int SIZE)
+//{
+//    for (int i=0; i < SIZE; i++) {
+//        cout << myLetters[i] << endl;
+//    }
+//}
